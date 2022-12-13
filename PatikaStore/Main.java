794abb9403e49363720855fc50f8d3cd7d3dd595
Cem Fidan <112818050+cemfidan@users.noreply.github.com>
2022-12-13 14:29:10 +0300

    Update Main.java

--- PatikaStore/Main.java
+++ PatikaStore/Main.java
@@ -1,11 +1,10 @@
 public class Main {
+	
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		
-		MainHelper mHelper = new MainHelper();
-		
-		mHelper.run();
+        Store store = new Store();
 
-	}
+        store.run();
 
+    }
 }
